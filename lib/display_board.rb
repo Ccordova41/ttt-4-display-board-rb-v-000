73dc564432a5459c7b3dff4e0825365b5a0e4057
Catherine Cordova <ccordova41@gmail.com>
2018-04-20 19:04:41 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 board = [" "," "," "," "," "," "," "," "," "]
 rows = ["   |   |   ", "-----------\n   |   |   \n -----------", "   |   |   "]
 
-def display_board(rows)
+def display_board(rows = board)
 
 puts #{rows} 
 end 
